ea443fdff8b661c5547274293523c5861776fe64
Zeid Al-Ameedi <zeid.al-ameedi@wsu.edu>
2019-09-07 21:02:39 -0700

    Linear Regression model complete

--- ML/regression.py
+++ ML/regression.py
@@ -10,6 +10,8 @@ import tensorflow
 import keras
 import numpy
 import sklearn
+from sklearn import model_selection
+from sklearn import linear_model
 
 
 def goog_stock():
@@ -25,10 +27,26 @@ def goog_stock():
 
 
 data = pd.read_csv('student-mat.csv', sep=";")
-data = data[['G1', 'G2', 'G3','studytime', 'absences', 'failures']]
+data = data[['G1', 'G2', 'G3', 'studytime', 'absences', 'failures']]
 
 predict = "G3"
 X = numpy.array(data.drop([predict], 1))
 Y = numpy.array(data[predict])
 
-x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(
+    X, Y, test_size=0.1)
+
+linear = linear_model.LinearRegression()
+linear.fit(x_train, y_train)
+accuracy = linear.score(x_test, y_test)
+
+# Model outcome
+print("\n")
+print(accuracy)  # This is the accuracy! Based on the attributes we provided
+print("Co: " + str(linear.coef_))
+print("Intercept: " + str(linear.intercept_))
+
+stud_prediction = linear.predict(x_test)
+
+for x in range(len(stud_prediction)):
+    print(stud_prediction[x], x_test[x], y_test[x])
